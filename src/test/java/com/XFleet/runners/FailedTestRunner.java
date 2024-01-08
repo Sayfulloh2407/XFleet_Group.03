5ebedac5513673196ae6568a798199d5f2502e50
Sayfulloh2407 <abdurazzoqzodasayfullo@gmail.com>
2024-01-08 22:00:00 +0100

    Initial commit

---
+++ src/test/java/com/XFleet/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.XFleet.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/XFleet/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
