cb7e0b6e3f5ad430cf304e9f2241a8dbfdd7ce80
aygulbertiz <140380702+aygulbertiz@users.noreply.github.com>
2024-01-15 13:41:33 +0100

    chnaged my code and add json

--- src/test/java/com/XFleet/runners/CukesRunner.java
+++ src/test/java/com/XFleet/runners/CukesRunner.java
@@ -8,13 +8,14 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "html:target/cucumber-reports.html",
+                "json:target/cucumber.json",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+              //  "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
         glue = "com/XFleet/step_definitions",
         dryRun = true,
-        tags = "@wip",
+        tags = "@B31G3-282",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
